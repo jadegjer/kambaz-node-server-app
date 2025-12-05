c7cf26e6ba45baf2171439934c9b794a8898ab12
Jade Gjermundsen <71979908+jadegjer@users.noreply.github.com>
2025-12-05 14:02:42 -0500

    Add files via upload

---
+++ Kambaz/Users/schema.js
@@ -0,0 +1,26 @@
+import model from "./model.js";
+import { v4 as uuidv4 } from "uuid";
+
+export default function UsersDao() {
+  const createUser = (user) => {
+    user._id = uuidv4();  // Generate a unique ID
+    return model.create(user);
+  };
+  
+  const findAllUsers = () => model.find();
+  const findUserById = (userId) => model.findById(userId);
+  const findUserByUsername = (username) => model.findOne({ username: username });
+  const findUserByCredentials = (username, password) => model.findOne({ username, password });
+  const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+  const deleteUser = (userId) => model.deleteOne({ _id: userId });
+  
+  return { 
+    createUser, 
+    findAllUsers, 
+    findUserById, 
+    findUserByUsername, 
+    findUserByCredentials, 
+    updateUser, 
+    deleteUser 
+  };
+}
